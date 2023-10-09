405d58f8150cf3942c389a1ea4e240c61d3740d7
jeffersonwalker45 <106553745+jeffersonwalker45@users.noreply.github.com>
2023-10-09 14:46:30 +0000

    kjnoij

--- main2.js
+++ main2.js
@@ -1,4 +1,10 @@
 function tocasomclap(){
     document.querySelector('#som_tecla_clap').play()
 }
+function tocasompom () {
+    document.querySelector('#som_tecla_pom').play();
+    }
+    
+document.querySelector('.tecla_pom').onclick = tocasompom;
 document.querySelector('.tecla_clap').onclick = tocasomclap
+document.querySelector('.tecla_tim').onclick = tocasomtim
