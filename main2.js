15e7b2135b880751e6ef434b84e0407c2832209e
jeffersonwalker45 <106553745+jeffersonwalker45@users.noreply.github.com>
2023-10-09 14:49:03 +0000

    dfhfh

--- main2.js
+++ main2.js
@@ -1,10 +1,12 @@
 function tocasomclap(){
-    document.querySelector('#som_tecla_clap').play()
+    document.querySelector('#som_tecla_clap').play();
 }
 function tocasompom () {
     document.querySelector('#som_tecla_pom').play();
-    }
-    
+}
+function tocasomtim () {
+    document.querySelector('#som_tecla_tim').play();
+}
 document.querySelector('.tecla_pom').onclick = tocasompom;
-document.querySelector('.tecla_clap').onclick = tocasomclap
-document.querySelector('.tecla_tim').onclick = tocasomtim
+document.querySelector('.tecla_clap').onclick = tocasomclap;
+document.querySelector('.tecla_tim').onclick = tocasomtim;
