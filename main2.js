9e6432ab4379b3e89d01601ec66fc8b9403f7633
jeffersonwalker45 <106553745+jeffersonwalker45@users.noreply.github.com>
2023-10-16 11:22:06 -0300

    Update main2.js
    bla bla

--- main2.js
+++ main2.js
@@ -1,12 +1,37 @@
+const jeffinho = document.querySelector;
 function tocasomclap(){
-    document.querySelector('#som_tecla_clap').play();
+    jeffinho('#som_tecla_clap').play();
 }
-function tocasompom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocasompom (){
+    jeffinho('#som_tecla_pom').play();
 }
-function tocasomtim () {
-    document.querySelector('#som_tecla_tim').play();
+function tocasomtim (){
+    jeffinho('#som_tecla_tim').play();
 }
-document.querySelector('.tecla_pom').onclick = tocasompom;
-document.querySelector('.tecla_clap').onclick = tocasomclap;
-document.querySelector('.tecla_tim').onclick = tocasomtim;
+function tocasompuff (){
+    jeffinho('#som_tecla_puff').play();
+}
+function tocasomsplash (){
+    jeffinho('#som_tecla_splash').play();
+}
+function tocasomtoim (){
+    jeffinho('#som_tecla_toim').play();
+}
+function tocasompsh (){
+    jeffinho('#som_tecla_psh').play();
+}
+function tocasomtic (){
+    jeffinho('#som_tecla_tic').play();
+}
+function tocasomtom (){
+    jeffinho('#som_tecla_tom').play();
+}
+jeffinho('.tecla_pom').onclick = tocasompom;
+jeffinho('.tecla_clap').onclick = tocasomclap;
+jeffinho('.tecla_tim').onclick = tocasomtim;
+jeffinho('.tecla_puff').onclick = tocasompuff;
+jeffinho('.tecla_splash').onclick = tocasomsplash;
+jeffinho('.tecla_toim').onclick = tocasomtoim;
+jeffinho('.tecla_psh').onclick = tocasompsh;
+jeffinho('.tecla_tic').onclick = tocasomtic;
+jeffinho('.tecla_tom').onclick = tocasomtom;
